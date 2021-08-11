397feb94741c9c3cd92d7251094aac9381a38314
venkatathota <itvenkata@gmail.com>
2021-08-10 20:49:16 -0500

    added H2 TEST CONFIG

---
+++ src/test/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO `EMPLOYEE` VALUES
+     (1,'PeterM','ABC123abc*','peter@email.com','400 W'),
+     (2,'Mike','password','mike@email.com','500 W'),
+     (3,'KingPeter','password','kingpeter@email.com','600 W');
